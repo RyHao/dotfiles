379f366de3eab82d288b4d2174bd5f95629511a3
RyHao <kinghao914@gmail.com>
2017-12-25 17:23:17 +0800

    add yarn bin and n bin to PATH env

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,2 +1,10 @@
 source ~/.zsh/plugin.zsh
 source ~/.zsh/theme.zsh
+
+export ANDROID_HOME=${HOME}/Library/Android/sdk
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+
+export PATH=${PATH}:~/.yarn/bin
+
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
