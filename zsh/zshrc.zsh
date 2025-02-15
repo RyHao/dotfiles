382a1da661eceb6f3d359691fc3310ed26320d8d
Ryan Lyu <ryhao.tw@gmail.com>
2025-02-15 23:04:24 +0800

    feat: modify zshrc config

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -6,5 +6,8 @@ export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
 export PATH=${PATH}:~/.yarn/bin
+export PATH=${PATH}:~/.cargo/bin
 
 export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+
+alias lg='lazygit'
